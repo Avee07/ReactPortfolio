b66b395ef88ad5281085fe8069cd935dcbc8f492
Arwinder Singh <arwinder7024@gmail.com>
2024-04-06 00:11:01 +0530

    update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,36 +2,42 @@ import React from "react";
 import Workitem from "./workitem";
 
 const data = [
-    {
-		"year": 2010,
-		"title": "Workitem	",
-		"duration": '6 years',
-		"details": "Sed nec metus facilisis lorem tristique aliquet. Phasellus fermentum convallis"
-	},
-	{
-		"year": 2201,
-		"title": "Google",
-		"duration": "10 years",
-		"details": "lacus pede sagittis augue, eu tempor erat neque non quam."
-	},
-	{
-		"year": 2001,
-		"title": "Facebook",
-		"duration": '7 years',
-		"details": "Curae Donec tincidunt. Donec vitae erat vel pede blandit congue."
-	},
-	{
-		"year": 2210,
-		"title": "Instagram",
-		"duration": '12 years',
-		"details": "ligula. Donec luctus aliquet odio. Etiam ligula tortor, dictum eu,"
-	},
-	{
-		"year": 2031,
-		"title": "Infosys,",
-		"duration": ' 3 years',
-		"details": "Mauris magna. Duis dignissim tempor arcu. Vestibulum ut eros non"
-	}
+  {
+    year: "08/2023  -  present",
+    title: "RR ISPAT (A Unit Of GPIL)	",
+    position: "Graduate Engineer Trainee: Flutter Developer",
+    details: [
+      "Collaborate with cross-functional teams to design, develop, and maintain web and mobile applications.",
+      "Assisted in the development of new features and enhancements for existing mobile apps.",
+      "Delivered presentations to workers to explain the application and updates on projects.",
+      "Collaborated with development teams, internal customers, and product line management to verify delivery of desired quality requirements to distributors.",
+      "Worked closely with engineers on projects.",
+    ],
+  },
+  {
+    year: "02/2023 - 08/2023",
+    title: "Nava Raipur Atal Nagar Smart City Corporaation Ltd",
+    position: "IT Intern",
+    details: [
+      "Helped in designing and developing the Nava Raipur Atal Nagar website.",
+      "Worked as a Support engineer in IT-related functions like ICCC and other	digital platforms",
+      "Performed such other duties as the supervisor may deem necessary from time to time.",
+      "Coordinated in organizing technical and other events like Yoga Day and Smart City anniversary.",
+    ],
+  },
+
+  {
+    year: "07/2022 - 08/2022",
+    title: "Twowaits Technologies Pvt. Ltd",
+    position: "Web Development Scholar Intern",
+    details: [
+      "Developed user interfaces and user experiences for web applications",
+      "Collaborated with teammates and updated application versions using Git",
+      "Used best practices in HTML and CSS to develop a static site based on a static image of a website design",
+      "Created custom web-based applications utilizing HTML, CSS, JavaScript, and other technologies",
+      "Developed cross-browser/platform HTML5, CSS, and JavaScript to match design specs for complex page layouts while adhering to code standards",
+    ],
+  },
 ];
 
 const Work = () => {
@@ -44,7 +50,7 @@ const Work = () => {
             key={idx}
             year={item.year}
             title={item.title}
-            duration={item.duration}
+            position={item.position}
             details={item.details}
           />
         ))}
